62667d4568a4e0e7ae43368a1799d8841bb82ad3
jonty-comp <jontysewell@gmail.com>
2013-05-15 17:10:51 +0100

    sample playout app base layout...missing most of the stuff

--- main.py
+++ main.py
@@ -4,21 +4,42 @@ Test Audiowall application.
 '''
 
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from dps.audiowall.page import AudiowallPage
 
 from glob import glob
+import json
+import uuid
 
-import globals   
+import globals
 
 class TestApp(App):
     def build(self):
-        self.root = root = AudiowallPage()
+        self.root = BoxLayout(orientation='horizontal')
+        self.leftbox = BoxLayout(size_hint=(.6,1))
+        self.root.add_widget(self.leftbox)
+
+        self.rightbox = BoxLayout(orientation='vertical', size_hint=(.4,1))
+        self.root.add_widget(self.rightbox)
+
+        self.primary_wall = AudiowallPage()
         i = 0
         for fn in glob('/home/jonty/src/beemo/testaudio/*'):
             print "%i - %s" % (i, fn)
-            root.buttons[i].filename = fn
+            self.primary_wall.buttons[i].filename = fn
             i += 1
-        return root
+
+        self.rightbox.add_widget(self.primary_wall)
+
+        self.secondary_wall = AudiowallPage()
+        i = 0
+        for fn in glob('/home/jonty/src/beemo/testaudio/*'):
+            print "%i - %s" % (i, fn)
+            self.secondary_wall.buttons[i].filename = fn
+            i += 1
+            
+        self.rightbox.add_widget(self.secondary_wall)
+        return self.root
 
 if __name__ == '__main__':
     globals.init()
