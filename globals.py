7664aae1824a2a581c2cf5b1a7520e427ca23345
jonty-comp <jontysewell@gmail.com>
2013-05-16 17:10:29 +0100

    this global should default to True

--- globals.py
+++ globals.py
@@ -4,4 +4,4 @@ def init():
 	# defines whether an audio thread is allowed to start loading content or not.
 	# if it wants to but available is false, it will keep checking until it can!
 	global _available
-	_available = False
+	_available = True
