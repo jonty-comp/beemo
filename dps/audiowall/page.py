280b2052ef44badc6e2b4c0b86fbd760aa68553f
jonty-comp <jontysewell@gmail.com>
2013-05-16 17:08:52 +0100

    Added page labels to AudiowallPages

--- dps/audiowall/page.py
+++ dps/audiowall/page.py
@@ -3,23 +3,40 @@ Audiowall Page. Made up of AudiowallButtons.
 ============================================
 '''
 
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.properties import StringProperty
 from dps.audiowall.item import AudiowallItem
 
 import globals
 
-class AudiowallPage(GridLayout):
+class AudiowallPage(BoxLayout):
+
+    name = StringProperty(None)
 
     def __init__(self, **kwargs):
         super(AudiowallPage, self).__init__(**kwargs)
-        self.cols = 3
-        globals._available = True
-        self.buttons = []
-        self.register_event_type('on_add_item')
-        for i in range(0,12,1):
-            self.dispatch('on_add_item')
 
-    def on_add_item(self, *largs):
+        self.orientation = 'vertical'
+        self.buttons = []
+
+        self.label = Label()
+        self.label.size_hint = (1,0.1)
+        self.add_widget(self.label)
+
+        self.grid = GridLayout()
+        self.grid.cols = 3
+        self.add_widget(self.grid)
+
+        for i in range(0,12,1):
+            self.add_item()
+
+    def add_item(self, *largs):
         btn = AudiowallItem()
-        self.add_widget(btn)
+        self.grid.add_widget(btn)
         self.buttons.append(btn)
+
+    def on_name(self, instance, name):
+        print self.name
+        self.label.text = self.name
